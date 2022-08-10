521db822fa327bb30e6c81da17444782389223ed
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2022-08-10 18:08:58 +0530

    Implement most opcodes
    TODO: Trap instructions

--- src/vm.rs
+++ src/vm.rs
@@ -53,7 +53,12 @@ impl Vm {
             let op: Opcode = (inst >> 12).try_into().unwrap();
 
             match op {
-                Opcode::Br => todo!(),
+                Opcode::Br => {
+                    let nzp = inst >> 9 & 0b111;
+                    if nzp & (self.psr & 0b111) != 0 {
+                        self.pc += sign_ext(inst, 9);
+                    }
+                }
                 Opcode::Add => {
                     let dr = (inst >> 9 & 0b111) as usize;
                     let sr1 = (inst >> 6 & 0b111) as usize;
@@ -68,20 +73,94 @@ impl Vm {
 
                     self.set_cc(dr);
                 }
-                Opcode::Ld => todo!(),
-                Opcode::St => todo!(),
-                Opcode::Jsr => todo!(),
-                Opcode::And => todo!(),
-                Opcode::Ldr => todo!(),
-                Opcode::Str => todo!(),
-                Opcode::Rti => todo!(),
-                Opcode::Not => todo!(),
-                Opcode::Ldi => todo!(),
-                Opcode::Sti => todo!(),
-                Opcode::Ret => todo!(),
-                Opcode::Reserved => todo!(),
-                Opcode::Lea => todo!(),
-                Opcode::Trap => todo!(),
+                Opcode::Ld => {
+                    let dr = (inst >> 9 & 0b111) as usize;
+
+                    self.reg[dr] = self.read_mem(self.pc + sign_ext(inst, 9));
+                    self.set_cc(dr);
+                }
+                Opcode::St => {
+                    let sr = (inst >> 9 & 0b111) as usize;
+
+                    self.write_mem(self.pc + sign_ext(inst, 9), self.reg[sr]);
+                }
+                Opcode::Jsr => {
+                    let temp = self.pc;
+                    self.pc = if inst & (1 << 11) != 0 {
+                        self.pc + sign_ext(inst, 11)
+                    } else {
+                        inst >> 6 & 0b111
+                    };
+
+                    self.reg[7] = temp;
+                }
+                Opcode::And => {
+                    let dr = (inst >> 9 & 0b111) as usize;
+                    let sr1 = (inst >> 6 & 0b111) as usize;
+
+                    if inst & (1 << 5) != 0 {
+                        let imm5 = sign_ext(inst, 5);
+                        self.reg[dr] = self.reg[sr1] & imm5;
+                    } else {
+                        let sr2 = (inst & 0b111) as usize;
+                        self.reg[dr] = self.reg[sr1] & self.reg[sr2];
+                    }
+
+                    self.set_cc(dr);
+                }
+                Opcode::Ldr => {
+                    let dr = (inst >> 9 & 0b111) as usize;
+                    let br = (inst >> 6 & 0b111) as usize;
+
+                    let addr = self.reg[br] + sign_ext(inst, 6);
+                    self.reg[dr] = self.read_mem(addr);
+
+                    self.set_cc(dr);
+                }
+                Opcode::Str => {
+                    let sr = (inst >> 9 & 0b111) as usize;
+                    let br = (inst >> 6 & 0b111) as usize;
+
+                    let addr = self.reg[br] + sign_ext(inst, 6);
+                    self.write_mem(addr, self.reg[sr]);
+                }
+                Opcode::Not => {
+                    let dr = (inst >> 9 & 0b111) as usize;
+                    let sr1 = (inst >> 6 & 0b111) as usize;
+
+                    self.reg[dr] = !self.reg[sr1];
+
+                    self.set_cc(dr);
+                }
+                Opcode::Ldi => {
+                    let dr = (inst >> 9 & 0b111) as usize;
+                    let addr = self.read_mem(self.pc + sign_ext(inst, 9));
+
+                    self.reg[dr] = self.read_mem(addr);
+                    self.set_cc(dr);
+                }
+                Opcode::Sti => {
+                    let sr = (inst >> 9 & 0b111) as usize;
+                    let addr = self.read_mem(self.pc + sign_ext(inst, 9));
+
+                    self.write_mem(addr, self.reg[sr]);
+                }
+                Opcode::Jmp => {
+                    let br = (inst >> 6 & 0b111) as usize;
+                    self.pc = self.reg[br];
+                }
+                Opcode::Lea => {
+                    let dr = (inst >> 9 & 0b111) as usize;
+                    let addr = self.pc + sign_ext(inst, 9);
+
+                    self.reg[dr] = addr;
+                    self.set_cc(dr);
+                }
+                Opcode::Trap => {
+                    // implement traps in assembly or rust?
+                    todo!()
+                }
+                Opcode::Rti | Opcode::Reserved => unimplemented!("Bad opcode"),
             }
         }
     }
@@ -122,23 +201,24 @@ impl Default for Vm {
     }
 }
 
+#[allow(dead_code)]
 enum Opcode {
-    Br = 0,
-    Add,
-    Ld,
-    St,
-    Jsr,
-    And,
-    Ldr,
-    Str,
-    Rti,
-    Not,
-    Ldi,
-    Sti,
-    Ret,
-    Reserved,
-    Lea,
-    Trap,
+    Br = 0b0000,
+    Add = 0b0001,
+    Ld = 0b0010,
+    St = 0b0011,
+    Jsr = 0b0100,
+    And = 0b0101,
+    Ldr = 0b0110,
+    Str = 0b0111,
+    Rti = 0b1000,
+    Not = 0b1001,
+    Ldi = 0b1010,
+    Sti = 0b1011,
+    Jmp = 0b1100,
+    Reserved = 0b1101,
+    Lea = 0b1110,
+    Trap = 0b1111,
 }
 
 #[derive(Debug)]
